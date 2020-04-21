d1668980814a48667fc1a1a271d0e1697a31d1f7
liyufeng <liyufeng.2020@outlook.com>
2020-04-21 15:28:12 +0900

    fist commit

---
+++ src/main/java/hello/spring/mvc/echo/EchoController.java
@@ -0,0 +1,36 @@
+package hello.spring.mvc.echo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EchoController {
+
+    @ModelAttribute
+    public EchoForm setUpEchoForm() {
+        EchoForm form = new EchoForm();
+        return form;
+    }
+
+    @RequestMapping
+    public String index(Model model) {
+        return "echo/input";
+    }
+
+    @RequestMapping(value = "echo/echo", method = RequestMethod.POST)
+    public String echo(@Validated EchoForm form, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            return "echo/input";
+        }
+
+        model.addAttribute("name", form.getName());
+        return "echo/echo";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view/echo/echo.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ page pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Echo Application</title>
+</head>
+<body>
+  <p>
+    Hello <c:out value="${name}" /> <%-- (1) --%>
+  </p>
+</body>
+</html>
